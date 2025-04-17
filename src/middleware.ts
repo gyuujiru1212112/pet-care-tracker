dc5c198067d5a6be2287813438f7ddb848ac8cce
gyuujiru1212112 <ydjing121@gmail.com>
2025-04-17 18:19:43 -0400

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,48 +1,14 @@
-// middleware.ts
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { auth } from "@/lib/auth";
 
-export async function middleware(req: NextRequest) {
-  const token = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-    secureCookie: true,
-  });
-
-  const cookieName =
-    process.env.NODE_ENV === "production"
-      ? "__Secure-next-auth.session-token"
-      : "next-auth.session-token";
-
-  console.log("Raw cookie value:", req.cookies.get(cookieName));
-
-  console.log("Token in middleware:", token);
-  const isAuth = !!token;
-  const isLoginPage = req.nextUrl.pathname === "/login";
-  const isSignupPage = req.nextUrl.pathname === "/signup";
-
-  if (!isAuth) {
-    if (!isLoginPage && !isSignupPage) {
-      // no session and not at signup/login page
-      return NextResponse.redirect(new URL("/login", req.url));
-    }
-  } else if (isAuth) {
-    if (isSignupPage || isLoginPage) {
-      // has session but at signup/login page
-      return NextResponse.redirect(new URL("/dashboard", req.url));
-    }
+export default auth((req) => {
+  // req.auth
+  if (!req.auth && req.nextUrl.pathname !== "/login") {
+    const newUrl = new URL("/login", req.nextUrl.origin);
+    return Response.redirect(newUrl);
   }
+});
 
-  return NextResponse.next();
-}
-
+// Optionally, don't invoke Middleware on some paths
 export const config = {
-  matcher: [
-    "/signup/:path*",
-    "/login/:path*",
-    "/dashboard/:path*",
-    "/pets/:path*",
-    "/api/:path*",
-  ],
+  matcher: ["/dashboard/:path*", "/pets/:path*"],
 };
