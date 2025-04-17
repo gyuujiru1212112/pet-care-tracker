d4a3214a46e66905c1393b83c7f4a8a26cb3c19e
gyuujiru1212112 <ydjing121@gmail.com>
2025-04-17 18:27:42 -0400

    check error

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,9 @@
 import { auth } from "@/lib/auth";
 
 export default auth((req) => {
+  console.log("Auth check: ", req.auth); // Log the auth object to see if it is being set correctly
+  console.log("Request Pathname: ", req.nextUrl.pathname); // Log the requested pathname
+
   // req.auth
   if (!req.auth && req.nextUrl.pathname !== "/login") {
     console.log("Enter");
