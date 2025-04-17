5f1614c09b9470da0a67d98695527f95285e4c1c
gyuujiru1212112 <ydjing121@gmail.com>
2025-04-17 19:27:06 -0400

    try again

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,52 @@
-import { auth } from "@/lib/auth";
+import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export default auth((req) => {
-  console.log("Auth check: ", req.auth);
-  console.log("Request Pathname: ", req.nextUrl.pathname);
+export async function middleware(req: NextRequest) {
+  const cookieKey =
+    process.env.NODE_ENV === "production"
+      ? "__Secure-authjs.session-token"
+      : "authjs.session-token";
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+    salt: cookieKey,
+    cookieName: cookieKey,
+  });
 
-  if (
-    !req.auth &&
-    req.nextUrl.pathname !== "/login" &&
-    req.nextUrl.pathname !== "/signup"
-  ) {
-    const newUrl = new URL("/login", req.nextUrl.origin);
-    return NextResponse.redirect(newUrl);
-  }
+  const cookieName =
+    process.env.NODE_ENV === "production"
+      ? "__Secure-next-auth.session-token"
+      : "next-auth.session-token";
 
-  if (
-    req.auth &&
-    (req.nextUrl.pathname === "/login" || req.nextUrl.pathname === "/signup")
-  ) {
-    const newUrl = new URL("/dashboard", req.nextUrl.origin);
-    return NextResponse.redirect(newUrl);
+  console.log("Raw cookie value:", req.cookies.get(cookieName));
+
+  console.log("Token in middleware:", token);
+  const isAuth = !!token;
+  const isLoginPage = req.nextUrl.pathname === "/login";
+  const isSignupPage = req.nextUrl.pathname === "/signup";
+
+  if (!isAuth) {
+    if (!isLoginPage && !isSignupPage) {
+      // no session and not at signup/login page
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  } else if (isAuth) {
+    if (isSignupPage || isLoginPage) {
+      // has session but at signup/login page
+      return NextResponse.redirect(new URL("/dashboard", req.url));
+    }
   }
 
   return NextResponse.next();
-});
+}
 
 export const config = {
-  matcher: ["/login", "/signup", "/dashboard", "/pets/:path*"],
-  runtime: "nodejs",
+  matcher: [
+    "/signup/:path*",
+    "/login/:path*",
+    "/dashboard/:path*",
+    "/pets/:path*",
+    "/api/:path*",
+  ],
 };
