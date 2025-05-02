84d0d98c859eec94bb1845001ec5e062f818993f
gyuujiru1212112 <ydjing121@gmail.com>
2025-05-02 17:49:09 -0400

    fix an error

--- src/app/api/pets/route.ts
+++ src/app/api/pets/route.ts
@@ -1,9 +1,25 @@
+import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const pets = await prisma.pet.findMany();
+    const session = await auth();
+
+    if (!session) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const userId = session.user?.id;
+    if (!userId) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const pets = await prisma.pet.findMany({
+      where: { userId: userId },
+      orderBy: { createdAt: "asc" },
+    });
+
     if (!pets) {
       return NextResponse.json({ error: "Pets not found" }, { status: 404 });
     }
